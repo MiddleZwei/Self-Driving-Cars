Fix F Jacobian matrix: remove delta t

state_estimation_localization/final_project/es_ekf.py
@@ -190,9 +190,9 @@ for k in range(1, imu_f.data.shape[0]):  # start at 1 b/c we have initial predic
     # 2. Propagate uncertainty
     F = np.identity(9)
     F[:3, 3:6] = np.identity(3) * delta_t
-    F[3:6, 6:] = -(C_ns @ skew_symmetric(imu_f.data[k - 1].reshape((3, 1)))) * delta_t
+    F[3:6, 6:] = -(C_ns @ skew_symmetric(imu_f.data[k-1].reshape((3,1))))
 
-    Q = np.eye(6)
+    Q = np.identity(6)
     Q[0:3, 0:3] *= var_imu_f
     Q[3:, 3:] *= var_imu_w
     Q *= delta_t**2
@@ -200,12 +200,12 @@ for k in range(1, imu_f.data.shape[0]):  # start at 1 b/c we have initial predic
     p_cov[k] = (F @ p_cov[k-1] @ F.T) + (l_jac @ Q @ l_jac.T)
 
     # 3. Check availability of GNSS and LIDAR measurements
-    if gnss.t.shape[0] > gnss_i and gnss.t[gnss_i] == imu_f.t[k]:
+    if gnss.t.shape[0] > gnss_i and gnss.t[gnss_i] == imu_f.t[k-1]:
         p_est[k], v_est[k], q_est[k], p_cov[k] = measurement_update(var_gnss, p_cov[k], gnss.data[gnss_i].T, p_est[k],
                                                                     v_est[k], q_est[k])
         gnss_i += 1
 
-    if  lidar.t.shape[0] > lidar_i and lidar.t[lidar_i] == imu_f.t[k]:
+    if lidar.t.shape[0] > lidar_i and lidar.t[lidar_i] == imu_f.t[k-1]:
         p_est[k], v_est[k], q_est[k], p_cov[k] = measurement_update(var_lidar, p_cov[k], lidar.data[lidar_i].T,
                                                                     p_est[k], v_est[k], q_est[k])
         lidar_i += 1